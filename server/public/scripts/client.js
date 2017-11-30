d56ba6c77c94c0ad30f41ff89165906431f0cfa9
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-30 10:56:13 -0600

    complete lecture

---
+++ server/public/scripts/client.js
@@ -0,0 +1,38 @@
+var app = angular.module('RestaurantApp', []);
+
+app.controller('FoodController', ['$http', function($http){
+    console.log('Food Controller has been loaded');
+    var self = this;
+    self.message = 'Zip zap partner!';
+    self.foodArray = [];
+
+    self.newFood = {is_hot: false};
+
+    self.getFood = function() {
+        $http({
+            method: 'GET',
+            url: '/food'
+        }).then(function(response) {
+            console.log('GET response', response.data);
+            self.foodArray = response.data;
+        })
+    };
+    
+
+    self.addNewFood = function(newFood) {
+        $http({
+            method: 'POST',
+            url: '/food',
+            data: newFood
+        }).then(function(response) {
+            console.log('POST response', response);
+            self.newFood = {is_hot: false};
+            self.getFood();
+        });
+    };
+
+
+
+    self.getFood();
+}]);
+
--- server/routes/food.js
+++ server/routes/food.js
@@ -23,4 +23,28 @@ router.get('/', function(req, res) {
     });
 });
 
+router.post('/', function(req, res) {
+    var newFood = req.body;
+    pool.connect(function (err, db, done) {
+        if (err) {
+            //error connecting to database
+            console.log('there was an error connecting: ', err);
+            res.sendStatus(500);
+        } else {
+            //connected to database
+            db.query(`INSERT INTO food (name, deliciousness_rating, is_hot)
+                    VALUES ($1, $2, $3);`, [newFood.name, newFood.deliciousness_rating, newFood.is_hot], 
+            function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
