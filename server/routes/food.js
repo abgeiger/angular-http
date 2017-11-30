cbce1b151d81e9bc5b5cc011946a6dc9235a3c8d
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-30 09:11:18 -0600

    spin up server with get req

--- server/routes/food.js
+++ server/routes/food.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../modules/pool');
 
 router.get('/', function(req, res) {
-    res.sendStatus(200);
-})
+    pool.connect(function (err, db, done) {
+        if (err) {
+            //error connecting to database
+            console.log('there was an error connecting: ', err);
+            res.sendStatus(500);
+        } else {
+            //connected to database
+            db.query('SELECT * FROM food;', function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
